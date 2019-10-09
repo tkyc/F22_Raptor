7ce3a0d0ccbc82deb79ba381f7b73f3808a74323
Terry <terrykyc.7000@gmail.com>
2019-10-08 20:09:43 -0700

    refactor complete

--- src/objects/Jet.js
+++ src/objects/Jet.js
@@ -1,6 +1,3 @@
-import * as THREE from 'three';
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
 class Jet {
 
     /**
@@ -10,28 +7,47 @@ class Jet {
      * @param {object} position - Position of model.
      * @param {object} rotation - Orientation of model.
      * @param {GLTFLoader} loader - Used to load the model.
+     * @param {function} callback - The callback function for when the model completes loading.
      */
-    constructor(modelPath, position, rotation, loader) {
-        this.position = position;
-        this.rotation = rotation;
+    constructor(modelPath, position, rotation, loader = null, callback = null) {
+        this.loader = loader;
+        this.callback = callback;
 
-        loader.load(modelPath, (gltf) => {
-            this.model = gltf.scene;
-            this.model.position.set(position.xpos, position.ypos, position.zpos);
-            this.model.rotation.set(rotation.xrot, rotation.yrot, rotation.zrot);
-        });
+        if (loader)
+            loader.load(modelPath, (gltf) => {
+                this.model = gltf.scene;
+                this.model.position.set(position.xpos, position.ypos, position.zpos);
+                this.model.rotation.set(rotation.xrot, rotation.yrot, rotation.zrot);
+                callback();
+            });
     }
 
-    // modelLoaderWrapper(modelPath, loader) {
-    //     return new Promise((resolve, reject) => {
-    //             if (gltf.scene) {
-    //                 resolve(gltf.scene);
-    //             } else { 
-    //                 console.log(`LoadError: Model failed to load - ${modelPath}`);
-    //                 reject(`LoadError: Model failed to load - ${modelPath}`);
-    //             }
-    //         });
-    // }
+    /**
+     * Changes jet's position.
+     * 
+     * @param {KeyboardEvent} event - Keyboard event.
+     */
+    moveJet(event) {
+        switch(event.keyCode) {
+            //UP
+            case 87:
+                this.model.position.z -= 0.01;
+                break;
+            //DOWN
+            case 83:
+                this.model.position.z += 0.01;
+                break;
+            //LEFT
+            case 65:
+                this.model.position.x -= 0.01;
+                break;
+            //RIGHT
+            case 68:
+                this.model.position.x += 0.01;
+                break;
+            default:
+        }
+    }
 }
 
 export default Jet;
