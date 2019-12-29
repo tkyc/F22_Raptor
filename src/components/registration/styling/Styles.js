60b7021695fb90de773eb87b4f4532115c04cc34
tkyc@arch <terrykyc.7000@gmail.com>
2019-12-28 22:16:11 -0800

    built registration forum

---
+++ src/components/registration/styling/Styles.js
@@ -0,0 +1,12 @@
+export const styles = {
+    container: {
+        marginTop: "5%",
+        position: "absolute",
+        left: "50%", 
+        transform: "translateX(-50%)" //Left and transform properties to centre an absolutely positioned element
+    },
+
+    grid: {
+        padding: "30px 30px 30px 30px"
+    }
+};
